a49f6e813ebb133a0ef04fc4ce8932107e2206cf
yurko_DMTR <4au.4au.4au@gmail.com>
2017-07-11 18:01:45 +0300

    gtgtgt

--- js/scripts/myscript.js
+++ js/scripts/myscript.js
@@ -1,1 +1,66 @@
 "use strict";
+
+var eggScript = function(){
+
+	/* remove placeholder */
+	var removePlaceholder = function () { 
+	    $('input,textarea').focus(function () {
+	        $(this).data('placeholder', $(this).attr('placeholder')).attr('placeholder', '');
+	    }).blur(function () {
+	        $(this).attr('placeholder', $(this).data('placeholder'));
+	    });
+	}
+
+	/* back to top */
+	var backToTopClick = function () {
+	    $('.back_to_top').click(function () {
+	        $('html,body').animate({
+	            scrollTop: 0
+	        }, 500);
+	        return false;
+	    });
+	}
+	var backToTopScroll = function () {
+	    if ($('.back_to_top').length > 0) {
+	        var scrollTop = $(window).scrollTop();
+	        if (scrollTop > 200) {
+	            $('.back_to_top').show();
+	        } else {
+	            $('.back_to_top').fadeOut();
+	        }
+	    }
+	}
+
+	/* smoothScroll to anchor */
+	var smoothScroll = function () {
+	    $('a[href*="#"]:not([href="#"])').click(function () {
+	        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	            var target = $(this.hash);
+	            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	            if (target.length) {
+	                $('html, body').animate({
+	                    scrollTop: target.offset().top
+	                }, 1000);
+	                return false;
+	            }
+	        }
+	    });
+	}
+
+
+
+	$(document).ready(function(){
+		removePlaceholder();
+		backToTopClick();
+		smoothScroll();
+
+		$('.select select').selectric();
+	});
+
+	$(window).scroll(function(){
+		backToTopScroll();
+	});
+}
+
+eggScript();
+
